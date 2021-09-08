9d9eca0a26287fcbc312d7718107ab1f7bb04656
zachlo89 <zachlo89@gmail.com>
2021-09-07 18:50:54 -0700

    Initial commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private CharacterController _charController;
+    private Transform _player;
+    [SerializeField] private float _speed = 2.0f;
+    private float _gravity = 20.0f;
+    private Vector3 _velocity;
+    
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (_charController.isGrounded == true)
+        {
+            Vector3 direction = _player.position - transform.position;
+            direction.Normalize(); // len 1 unit distance
+            direction.y = 0;
+            // rot twd player
+            transform.localRotation = Quaternion.LookRotation(direction);
+            _velocity = direction * _speed;
+        }
+
+        _velocity.y -= _gravity;
+        _charController.Move(_velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _charController;
+    
+    [Header(("Controller Settings"))]
+    [SerializeField] float _speed = 5.0f;
+    [SerializeField] private float _jumpHeight = 8.0f;
+    [SerializeField] private float _gravity = 20.0f;
+    
+    private Vector3 _direction;
+    private Vector3 _velocity;
+
+    //public float mouseX;
+    
+    private Camera _mainCam;
+    
+    [Header(("Camera Settings"))]
+    [SerializeField] private float _camSensitivity = 1.5f;
+    
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+        if (_charController == null)
+        {
+            Debug.LogError("Character Controller is NULL");
+        }
+        
+        _mainCam = Camera.main;
+        if (_mainCam == null)
+        {
+            Debug.LogError("Main Cam is NULL");
+        }
+        
+        // lock cursor & hide when game starts
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    void Update()
+    {
+        CalculateMovement();
+        CamController();
+        // tform local to world space Transform.TransformDirection
+        // velocity and tform dir to local dir
+        
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    
+    void CalculateMovement()
+    {
+        if (_charController.isGrounded == true)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            _direction = new Vector3(horizontal, 0, vertical);
+            _velocity = _direction * _speed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _velocity.y = _jumpHeight;
+            }
+        }
+        
+        // controller not grounded apply gravity
+        _velocity.y -= _gravity * Time.deltaTime;
+        
+        // tform local to world space Transform.TransformDirection
+        // velocity and tform dir to local dir
+        // move in dir facing
+        _velocity = transform.TransformDirection(_velocity);
+
+        _charController.Move(_velocity * Time.deltaTime);
+    }
+
+    
+    void CamController()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+            
+        // apply mouseX to player rot y (look L & R)
+        // transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + mouseX,
+        // transform.localEulerAngles.z);
+
+        Vector3 rotY = transform.localEulerAngles;
+        rotY.y += mouseX * _camSensitivity;
+            
+        //transform.localEulerAngles = rotY;
+        // alt for gimble lock
+        transform.localRotation = Quaternion.AngleAxis(rotY.y, Vector3.up);
+            
+        // apply mouseY to cam rot x (look UP & DOWN)
+        Vector3 currentCamRotation = _mainCam.gameObject.transform.localEulerAngles;
+        currentCamRotation.x -= mouseY * _camSensitivity;
+        _mainCam.gameObject.transform.localRotation = Quaternion.AngleAxis(currentCamRotation.x, Vector3.right); // rot around axis
+    }
+}
